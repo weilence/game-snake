7cf823ec847191d157c13270122a5dbddf83b5f4
Weilence <weilence@163.com>
2025-11-13 22:42:03 +0800

    feat: apple always spawn in space

--- global.gd
+++ global.gd
@@ -1,6 +1,6 @@
 extends Node
 
-const MAX_APPLE_COUNT = 5
+@export var MAX_APPLE_COUNT = 5
 const CELL_SIZE = 32
 @onready var size: Vector2i = get_viewport().get_visible_rect().size / CELL_SIZE
 
--- global_manager.gd
+++ global_manager.gd
@@ -4,7 +4,7 @@ var APPLE_SCENE = preload("res://apple.tscn")
 var SNAKE_SCENE = preload("res://snake.tscn")
 
 var score: int = 0
-var apples: Array[Vector2] = []
+var apples: Array[Vector2i] = []
 signal score_changed(new_score: int)
 signal game_over
 
@@ -31,7 +31,7 @@ func create_snake() -> void:
 	connect("score_changed", snake_node._on_game_manager_score_changed)
 	add_child(snake_node)
 
-func _on_snake_move_to(pos: Vector2) -> void:
+func _on_snake_move_to(pos: Vector2i) -> void:
 	var apple_index = apples.find(pos)
 	if apple_index != -1:
 		apples.remove_at(apple_index)
@@ -46,10 +46,40 @@ func _on_apple_spawn_timer_timeout() -> void:
 	if apples.size() >= Global.MAX_APPLE_COUNT:
 		return
 
-	var apple = Vector2(randi_range(0, int(Global.size.x - 1)), randi_range(0, int(Global.size.y - 1)))
+	var position = get_random_food_position(Global.size.x, Global.size.y)
+	if position == null:
+		print("No available position to spawn apple.")
+		return
+
+	var apple = Vector2i(randi_range(0, Global.size.x - 1), randi_range(0, Global.size.y - 1))
 	apples.append(apple)
 	print("Spawn apple at: ", apple)
 
 	var apple_node: Node2D = APPLE_SCENE.instantiate()
 	apple_node.position = apple * Global.CELL_SIZE
 	add_child(apple_node)
+
+func get_random_food_position(rows: int, cols: int) -> Variant:
+	var snake: Snake = $Snake
+	var dict: Dictionary = {
+		snake.head: true,
+	}
+	for body in snake.bodys:
+		dict[body] = true
+	for apple in apples:
+		dict[apple] = true
+
+	# 遍历所有格子
+	var available: Array[Vector2i] = []
+	for x in range(rows):
+		for y in range(cols):
+			var pos = Vector2i(x, y)
+			if pos not in dict:
+				available.append(pos)
+	
+	# 如果没有空位，就返回 null（比如蛇占满全图）
+	if available.is_empty():
+		return null
+	
+	# 从空位中随机选一个
+	return available.pick_random()
